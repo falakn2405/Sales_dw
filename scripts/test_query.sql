02c2b2441bab9d3884343607c950b680f233aab0
falakn2405 <falakn2405@gmail.com>
2026-03-14 18:00:58 +0500

    tables updated

--- scripts/test_query.sql
+++ scripts/test_query.sql
@@ -6,6 +6,26 @@ SELECT COUNT(*) FROM customer_info;
 
 SELECT * FROM product_info;
 
+SELECT COUNT(*) FROM product_info;
+
+SELECT * FROM sales_details;
+
+SELECT COUNT(*) FROM sales_details;
+
+SELECT * FROM customer_loc;
+
+SELECT COUNT(*) FROM customer_loc;
+
+SELECT * FROM customer_profile;
+
+SELECT COUNT(*) FROM customer_profile;
+
+SELECT * FROM product_category;
+
+SELECT COUNT(*) FROM product_category;
+
 TRUNCATE TABLE etl_log;
 
 SELECT * FROM etl_log;
+
+SHOW COLUMNS FROM customer_info;
---
+++ scripts/update_table.sql
@@ -0,0 +1,19 @@
+-- Adding one more column for new data info
+
+ALTER TABLE customer_info
+ADD COLUMN update_date DATETIME DEFAULT NOW();
+
+ALTER TABLE product_info
+ADD COLUMN update_date DATETIME DEFAULT NOW();
+
+ALTER TABLE sales_details
+ADD COLUMN update_date DATETIME DEFAULT NOW();
+
+ALTER TABLE customer_loc
+ADD COLUMN update_date DATETIME DEFAULT NOW();
+
+ALTER TABLE customer_profile
+ADD COLUMN update_date DATETIME DEFAULT NOW();
+
+ALTER TABLE product_category
+ADD COLUMN update_date DATETIME DEFAULT NOW();
